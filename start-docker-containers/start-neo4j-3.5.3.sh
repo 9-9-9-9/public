a77980af54442ec93a24c4197e67eb320e9c14b2
HungPV <hungpv@goquo.com>
2020-05-12 13:06:57 +0700

    'update'

--- start-docker-containers/start-neo4j-3.5.3.sh
+++ start-docker-containers/start-neo4j-3.5.3.sh
@@ -7,8 +7,8 @@ sudo docker run \
 	-d \
 	-p $NEO4J_HTTP_PORT:$NEO4J_HTTP_PORT \
 	-p $NEO4J_BOLT_PORT:$NEO4J_BOLT_PORT \
-	-v neo4j-v3:/data/db 
-	--env NEO4J_AUTH=neo4j/1234567 
+	-v neo4j-v3:/data/db \
+	--env NEO4J_AUTH=neo4j/1234567 \
 	neo4j:3.5.3
 
 nmap -sT 127.0.0.1 -p $NEO4J_HTTP_PORT
