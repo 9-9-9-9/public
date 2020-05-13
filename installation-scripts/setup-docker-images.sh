f4e6d987cb5893ffb97f1d528a5182868546fe00
HungPV <hungpv@goquo.com>
2020-05-13 20:05:21 +0700

    'update'

--- installation-scripts/setup-docker-images.sh
+++ installation-scripts/setup-docker-images.sh
@@ -2,22 +2,15 @@
 
 # Pull images
 echo 'Pulling neo4j images'
-docker pull neo4j:4.0.4
-docker pull neo4j:3.5.3
-echo 'Pulling postgres images'
-docker pull postgres:12.2
-docker pull postgres:11.7
-docker pull postgres:10.12
-docker pull postgres:9.6
+#docker pull neo4j:4.0.4
+#docker pull neo4j:3.5.3
+docker pull neo4j:3.5.17
 
-# Create volumes
-echo 'Creating volumes'
-docker volume create neo4j-v3
-docker volume create neo4j-v4
-docker volume create conf-neo4j-v3
-docker volume create conf-neo4j-v4
-docker volume create postgres
-docker volume create conf-postgres
+echo 'Pulling postgres images'
+#docker pull postgres:12.2
+#docker pull postgres:11.7
+#docker pull postgres:10.12
+docker pull postgres:9.6
 
 # Install additional tools
 sudo yum install nmap -y
