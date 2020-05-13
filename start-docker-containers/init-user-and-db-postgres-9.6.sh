77c82484dbc0088ea789f016c5d42da60f45e331
HungPV <hungpv@goquo.com>
2020-05-13 14:27:26 +0700

    'update'

--- start-docker-containers/init-user-and-db-postgres-9.6.sh
+++ start-docker-containers/init-user-and-db-postgres-9.6.sh
@@ -6,26 +6,31 @@ echo 'Setup postgres client'
 sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo yum install -y postgresql96
 
+# Create database
 PGPASSWORD=$POSTGRES_PWD createdb \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
 	$POSTGRES_DB_SALES_STRUCTURE
 
+# Revoke permissions on new database from any PUBLIC
 PGPASSWORD=$POSTGRES_PWD psql \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
 	--command="REVOKE ALL ON DATABASE $POSTGRES_DB_SALES_STRUCTURE FROM PUBLIC;"
 
+# Revoke permissions on default database from any PUBLIC
 PGPASSWORD=$POSTGRES_PWD psql \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
 	--command="REVOKE ALL ON DATABASE postgres FROM PUBLIC;"
 
+# Create user
 PGPASSWORD=$POSTGRES_PWD psql \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
 	--command="CREATE USER $POSTGRES_USER_SALES_STRUCTURE WITH PASSWORD '$POSTGRES_USERPASS_SALES_STRUCTURE';"
 
+# Grant permission on new database to above user
 PGPASSWORD=$POSTGRES_PWD psql \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
