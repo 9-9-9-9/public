d8e378b0b565d816d1179e96b85eaf97f07a226d
HungPV <hungpv@goquo.com>
2020-05-13 14:11:09 +0700

    'update'

--- start-docker-containers/init-user-and-db-postgres-9.6.sh
+++ start-docker-containers/init-user-and-db-postgres-9.6.sh
@@ -6,6 +6,11 @@ echo 'Setup postgres client'
 sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo yum install -y postgresql96
 
+PGPASSWORD=$POSTGRES_PWD psql \
+	--host=127.0.0.1 --port=5432 \
+	--username=postgres \
+	--command="REVOKE ALL ON postgres FROM PUBLIC;"
+
 PGPASSWORD=$POSTGRES_PWD createdb \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
