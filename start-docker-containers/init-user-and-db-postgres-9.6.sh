60a9eef6ab3ec02ffc3e55cf99d1daafb3769f34
HungPV <hungpv@goquo.com>
2020-05-13 13:53:41 +0700

    'update'

--- start-docker-containers/init-user-and-db-postgres-9.6.sh
+++ start-docker-containers/init-user-and-db-postgres-9.6.sh
@@ -6,8 +6,6 @@ echo 'Setup postgres client'
 sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo yum install -y postgresql96
 
-PGPASSWORD=$POSTGRES_PWD psql --host=127.0.0.1 --port=5432 --username=postgres
-
 PGPASSWORD=$POSTGRES_PWD createdb \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
@@ -16,4 +14,4 @@ PGPASSWORD=$POSTGRES_PWD createdb \
 PGPASSWORD=$POSTGRES_PWD psql \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
-	--command="CREATE USER '$POSTGRES_USER_SALES_STRUCTURE' WITH PASSWORD '$POSTGRES_USERPASS_SALES_STRUCTURE';"
+	--command="CREATE USER $POSTGRES_USER_SALES_STRUCTURE WITH PASSWORD '$POSTGRES_USERPASS_SALES_STRUCTURE';"
