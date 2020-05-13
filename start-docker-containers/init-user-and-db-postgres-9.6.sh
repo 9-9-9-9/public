d7db2d2ea34539e8f24c562479bcb8bb35bcec41
HungPV <hungpv@goquo.com>
2020-05-13 14:18:33 +0700

    'update'

--- start-docker-containers/init-user-and-db-postgres-9.6.sh
+++ start-docker-containers/init-user-and-db-postgres-9.6.sh
@@ -6,11 +6,6 @@ echo 'Setup postgres client'
 sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo yum install -y postgresql96
 
-PGPASSWORD=$POSTGRES_PWD psql \
-	--host=127.0.0.1 --port=5432 \
-	--username=postgres \
-	--command="REVOKE ALL ON DATABASE postgres FROM PUBLIC;"
-
 PGPASSWORD=$POSTGRES_PWD createdb \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
@@ -19,4 +14,14 @@ PGPASSWORD=$POSTGRES_PWD createdb \
 PGPASSWORD=$POSTGRES_PWD psql \
 	--host=127.0.0.1 --port=5432 \
 	--username=postgres \
+	--command="REVOKE ALL ON DATABASE $POSTGRES_DB_SALES_STRUCTURE FROM PUBLIC;"
+
+PGPASSWORD=$POSTGRES_PWD psql \
+	--host=127.0.0.1 --port=5432 \
+	--username=postgres \
 	--command="CREATE USER $POSTGRES_USER_SALES_STRUCTURE WITH PASSWORD '$POSTGRES_USERPASS_SALES_STRUCTURE';"
+
+PGPASSWORD=$POSTGRES_PWD psql \
+	--host=127.0.0.1 --port=5432 \
+	--username=postgres \
+	--command="GRANT ALL ON DATABASE $POSTGRES_DB_SALES_STRUCTURE TO $POSTGRES_USER_SALES_STRUCTURE;"
