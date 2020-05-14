8fb91f2b5a55625de0caaed93531f2529fed005a
HungPV <HungPV@vpbs.com.vn>
2020-05-14 18:41:35 +0700

    update

---
+++ installation-scripts/expose-ports.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+expose() {
+	firewall-cmd --zone=public --add-port=$1/tcp --permanent
+}
+
+if [[ "$(firewall-cmd --get-active-zones | grep public)" =~ 'public' ]]
+then
+	# Angular
+	expose 4200
+	
+	# Elastic Search
+	expose 9200
+	expose 9300
+	
+	# Neo4J
+	expose 7474
+	expose 7687
+	
+	# PostgreSQL
+	expose 5432
+	
+	firewall-cmd --reload
+elif
+	echo 'Can not find public zone'
+	exit 1
+fi
