75c61169039eb330ec867fd8e90810e757261832
HungPV <HungPV@vpbs.com.vn>
2020-05-14 16:32:37 +0700

    update

--- installation-scripts/setup-app-server.sh
+++ installation-scripts/setup-app-server.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-[ "$(which node)" -eq "/usr/bin/node" ] && echo 'Node already installed' && exit 1
+# install nodejs
+if [ "$(which node)" -eq "/usr/bin/node" ]
+then
+	echo 'Node already installed'
+else
+	cd /tmp
+	curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+	sudo yum install nodejs -y
 
-cd /tmp
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
-sudo yum install nodejs -y
+	[ "$(node --version)" != "v12.16.3" ] && echo 'Failure on installing nodejs v12.16.3 LTS' && exit 1
+fi
 
-[ "$(node --version)" != "v12.16.3" ] && echo 'Failure on installing nodejs v12.16.3 LTS' && exit 1
+# downgrade npm if version is 6.14.* which has bug with connection
+if [[ "$(npm --version)" =~ 6.13 ]]
+then
+	echo 'Good npm version'
+elif [[ "$(npm --version)" =~ 6.14 ]]
+	echo 'Current version of npm is 6.14.* has bug and should be downgraded'
+	sudo npm install -g @angular/cli
+fi
