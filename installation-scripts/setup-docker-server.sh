c376971da81e6b82f6ce46abda5ba40a7cdf7dc9
HungPV <hungpv@goquo.com>
2020-05-15 09:57:47 +0700

    'update'

--- installation-scripts/setup-docker-server.sh
+++ installation-scripts/setup-docker-server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+USERID=$1
+
+if [ -z "$USERID"]
+then
+	echo 'Must provide user id as first parameter (this user will be added to docker group)'
+	exit 1
+fi
+
 # System update
 sudo yum update -y
 
@@ -8,7 +16,7 @@ sudo yum install docker -y
 
 # Config to run docker without sudo
 sudo groupadd docker
-sudo gpasswd -a vpsadmin docker
+sudo gpasswd -a $USERID docker
 sudo service docker restart
 
 echo 'Please re-log'
